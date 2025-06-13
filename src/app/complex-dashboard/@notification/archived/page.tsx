110b73b83e45b54e0f841fe52488bb11b71a50a8
mentorvikash <mentorvikash@gmail.com>
2025-06-13 22:16:18 +0530

    add archived file

---
+++ src/app/complex-dashboard/@notification/archived/page.tsx
@@ -0,0 +1,12 @@
+// react component
+
+import Link from "next/link";
+
+export default function ArchivedNotificationPage() {
+  return (
+    <>
+      <div>ArchivedNotificationPage</div>
+      <Link href="/complex-dashboard">Back to Notification</Link>
+    </>
+  );
+}
--- src/app/complex-dashboard/@notification/page.tsx
+++ src/app/complex-dashboard/@notification/page.tsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
 import React from "react";
 
 function Notification() {
   return (
     <div>
       <h1>Notification Page</h1>
+      <Link href="/complex-dashboard/archived">Archived Notification</Link>
     </div>
   );
 }
